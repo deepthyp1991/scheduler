04e1d23e6d5637400b06c079393cbd7bdff05b28
deepthy <deepthyp1991@gmail.com>
2021-01-19 18:14:25 -0500

    First appointment views

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -10,7 +10,7 @@ export default function InterviewerList(props) {
       avatar = {value.avatar}
       name = {value.name}
       selected = {value.id === props.value}
-      setInterviewer={(event) => props.onChange(value.id)}				
+      setInterviewer={(event) => props.setInterviewer(value.id)}				
       />
     )
   })
