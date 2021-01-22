75e9ec0c0ce0f889cf1b7c3059c601c834ee3a30
deepthy <deepthyp1991@gmail.com>
2021-01-22 10:55:43 -0500

    used custom hooks in the Appointmtnet component

--- src/components/Application.js
+++ src/components/Application.js
@@ -6,7 +6,7 @@ import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "./Appointment"
 import {getAppointmentsForDay, getInterview} from "../helpers/selectors"
-
+// import useVisualMode from "hooks/useVisualMode"
 
 export default function Application(props) {
 
