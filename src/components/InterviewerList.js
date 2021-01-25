80dcf6520e6478ceea6143eeb7640ac03dffbad6
deepthy <deepthyp1991@gmail.com>
2021-01-25 14:04:44 -0500

    The first test with it and describe

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -2,6 +2,8 @@ import React from "react";
 import InterviewerListItem from "components/InterviewerListItem"
 import "components/InterviewerList.scss"
 
+import PropTypes from 'prop-types';
+
 export default function InterviewerList(props) {
   const interviewers = props.interviewers.map(value => {
     return (
@@ -14,6 +16,11 @@ export default function InterviewerList(props) {
       />
     )
   })
+
+  InterviewerList.propTypes = {
+    interviewers: PropTypes.array.isRequired
+  };
+  
 
   return (
     <section className="interviewers">
