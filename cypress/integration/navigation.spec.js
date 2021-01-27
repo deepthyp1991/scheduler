a59e5a035da988dd32515367fbbfbb424c20aaf4
deepthy <deepthyp1991@gmail.com>
2021-01-27 17:04:36 -0500

    Appointment tests with cypress

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -1,7 +1,10 @@
-const { CYCLIC_KEY } = require("@storybook/addon-actions/dist/constants")
-
 describe("Navigation", () => {
   it("should visit root", () => {
-    cy.visit("/")
+    cy.visit("/");
+  });
+  it("should navigate to tuesday", () => {
+    cy.contains("[data-testid=day]", "Tuesday")
+    .click()
+    .should("have.class", "day-list__item--selected")
   })
 })
